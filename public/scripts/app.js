82b7f8470829d365e5487b4d29c64bb0e33ea682
Paulyn <paulyntran@live.ca>
2019-02-20 23:50:01 +0000

    Successfully post/submit tweet and reset form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
+$(function() {
     
     const tweetData = [
         {
@@ -55,22 +55,24 @@ $(document).ready(function() {
 
     const createTweetElement = function(tweetData) {
         return `
-        <header class="tweet-header">
-        <img class="tweet-avatar" alt="Tweeter User Avatar" src=${tweetData.user.avatars.small}>
-        <span class="username">${tweetData.user.name}</span>
-        <span class="tweet-handle">${tweetData.user.handle}</span>
-      </header>
-      <section class="main-tweet">
-        <p>${tweetData.content.text}</p>
-      </section>
-      <footer class="tweet-footer">
-        <span class="date">${tweetData.created_at}</span>
-        <span class="engagement">
-          <a class="engage-link" href="#"><img class="sm-engage" alt="Like" src="/images/like.png"></a>
-          <a class="engage-link" href="#"><img class="sm-engage" alt="Retweer" src="/images/retweet-arrows-symbol.png"></a>
-          <a class="engage-link" href="#"><img class="sm-engage" alt="Flag Tweet" src="/images/flag-black-shape.png"></a>
-        </span>
-      </footer>
+            <article class="tweet">
+                <header class="tweet-header">
+                    <img class="tweet-avatar" alt="Tweeter User Avatar" src=${tweetData.user.avatars.small}>
+                    <span class="username">${tweetData.user.name}</span>
+                    <span class="tweet-handle">${tweetData.user.handle}</span>
+                </header>
+                <section class="main-tweet">
+                    <p>${tweetData.content.text}</p>
+                </section>
+                <footer class="tweet-footer">
+                    <span class="date">${tweetData.created_at}</span>
+                    <span class="engagement">
+                    <a class="engage-link" href="#"><img class="sm-engage" alt="Like" src="/images/like.png"></a>
+                    <a class="engage-link" href="#"><img class="sm-engage" alt="Retweer" src="/images/retweet-arrows-symbol.png"></a>
+                    <a class="engage-link" href="#"><img class="sm-engage" alt="Flag Tweet" src="/images/flag-black-shape.png"></a>
+                    </span>
+                </footer>
+            </article>
       `
     }
 
@@ -81,5 +83,23 @@ $(document).ready(function() {
     }
     
     renderTweets(tweetData);
+
+    $('#submitTweet').on('submit', function(event) {
+        event.preventDefault();
+        const $form = $('#submitTweet')
+        const data = $form.serialize();
+        console.log('tweet: ', data);
+        $.post('/tweets/', data)
+          .then((tweet) => {
+              console.log("TWEET from server", tweet)
+            const elm = createTweetElement(tweet)
+            $('.tweet-container').prepend(elm); 
+          })
+          .catch((err) => {
+            console.log(err);
+            alert('Something went wrong, please try again!');
+          });
+          this.reset();
+      });
 
 });
