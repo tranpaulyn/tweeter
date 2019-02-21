656048754b6d6a850f0fa5b2515aaa1aaed078f0
Paulyn <paulyntran@live.ca>
2019-02-21 00:39:06 +0000

    Successfully added loadTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,52 +6,7 @@
 
 $(function() {
     
-    const tweetData = [
-        {
-          "user": {
-            "name": "Newton",
-            "avatars": {
-              "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-              "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-              "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-            },
-            "handle": "@SirIsaac"
-          },
-          "content": {
-            "text": "If I have seen further it is by standing on the shoulders of giants"
-          },
-          "created_at": 1461116232227
-        },
-        {
-          "user": {
-            "name": "Descartes",
-            "avatars": {
-              "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-              "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-              "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-            },
-            "handle": "@rd" },
-          "content": {
-            "text": "Je pense , donc je suis"
-          },
-          "created_at": 1461113959088
-        },
-        {
-          "user": {
-            "name": "Johann von Goethe",
-            "avatars": {
-              "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-              "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-              "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-            },
-            "handle": "@johann49"
-          },
-          "content": {
-            "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-          },
-          "created_at": 1461113796368
-        }
-      ];
+    const tweetData = [];
 
     const createTweetElement = function(tweetData) {
         return `
@@ -102,6 +57,14 @@ $(function() {
           this.reset();
       });
 
-    $.getJSON('/tweets', )
+
+      const loadTweets = $.get('/tweets', function(tweets) {
+        console.log(tweets);
+        for (let content in tweets) {
+            console.log(tweets[content]);
+            const elm = createTweetElement(tweets[content]);
+            $('.tweet-container').append(elm);
+        }
+      })
 
 });
