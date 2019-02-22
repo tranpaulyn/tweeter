5797a4960eeef541b8ab4286266a161026b3040c
Paulyn <paulyntran@live.ca>
2019-02-22 16:45:51 +0000

    Added more comments to code and change material icons to font awesome icons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,15 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//  Cross Site Scripting for extra encoding
 function escape(str) {
-    var div = document.createElement('div');
+    let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   }
 
 
-
-    $(function() {
+// Create's front end elements for new tweets
+$(function() {
     const createTweetElement = function(tweetData) {
         return `
             <article class="tweet">
@@ -25,54 +26,56 @@ function escape(str) {
                     <p>${escape(tweetData.content.text)}</p>
                 </section>
                 <footer class="tweet-footer">
-                    <span class="date">${tweetData.created_at}</span>
+                    <span class="date">${new Date(tweetData.created_at).toLocaleString()}</span>
                     <span class="engagement">
                     <a class="engage-link" href="#"><img class="sm-engage" alt="Like" src="/images/like.png"></a>
                     <a class="engage-link" href="#"><img class="sm-engage" alt="Retweer" src="/images/retweet-arrows-symbol.png"></a>
                     <a class="engage-link" href="#"><img class="sm-engage" alt="Flag Tweet" src="/images/flag-black-shape.png"></a>
                     </span>
                 </footer>
-            </article>
-      `
+            </article>`
     }
 
+    // Render Tweets from the MongoDB
     function renderTweets(database) {
         for (let tweet in database) {
             $('.tweet-container').append(createTweetElement(database[tweet]));
         };
     }
 
+    // Compose button appear, hide and focus
     $('.composeTweet').click(function() {
         $('.new-tweet').slideToggle("slide");
         $('#text').focus();
     })
 
-
+    // Submit and prepend new tweet 
     $('#submitTweet').on('submit', function(event) {
         event.preventDefault();
         const $form = $('#submitTweet');
         const data = $form.serialize();
+        // Make sure tweet is within character limit
         if ($('.counter').html() >= 0) {
             $.post('/tweets/', data)
             .then((tweet) => {
                 const elm = createTweetElement(tweet)
                 $('.tweet-container').prepend(elm); 
                 this.reset();
-                $('.counter').html(140);
-                $('#serverAlert').hide();
+                $('.counter').html(140); // Reset Character Counter
+                $('#serverAlert').hide(); // If alerts appeared, get rid of them 
                 $('#exceedChar').hide();
-                $('.counter').css('color', '#244751');
+                $('.counter').css('color', '#244751'); // If character counter turned red, reset it
             })
             .catch((err) => {
                 console.log(err);
-                $('#serverAlert').show();
+                $('#serverAlert').show(); // Show server alert
             });
         } else {
-            $('#exceedChar').show();
+            $('#exceedChar').show(); // Show character exceed alert 
         }
       });
 
-
+      // Prepend tweets from the database 
       const loadTweets = function() {
           $.get('/tweets', function(tweets) {
             console.log(tweets);
@@ -83,7 +86,7 @@ function escape(str) {
             }
         })
     }
-
+    // Call function to prepend tweets
     loadTweets()
 
 });
